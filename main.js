6b5cda147a31dde34170143d04f5129f7f4a3ab3
BenEs16 <benjoest123@gmail.com>
2023-08-05 10:23:21 +0800

    First commit, first function added

---
+++ main.js
@@ -0,0 +1,26 @@
+function choosePhrase() {
+    let number = Math.floor(Math.random()*7);
+    switch (number){
+        case 0:
+            return "Call me asparagus!";
+            break;
+        case 1:
+            return "Let's go golfing!";
+            break;
+        case 2:
+            return "Gatorade!";
+            break;
+        case 3:
+            return "Tell them to bring out the whole ocean!";
+            break;
+        case 4: 
+            return "Let's go swimming!";
+            break;
+        case 5:
+            return "God did!";
+            break;
+        case 6:
+            return "Life is roblox";
+            break;
+    }
+}
