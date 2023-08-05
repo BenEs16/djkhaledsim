907ac1233432f4c12d1a8b25e73cdfed3458ac3d
BenEs16 <benjoest123@gmail.com>
2023-08-05 10:37:37 +0800

    Official Random QuoteGen v1.0

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function choosePhrase() {
 }
 
 function whoSaysIt() {
-    let num = Math.floor(Math.random()*3)
+    let num = Math.floor(Math.random()*4);
     switch (num){
         case 0:
             return "LeBron James";
@@ -37,5 +37,29 @@ function whoSaysIt() {
         case 2:
             return "Hanni from NewJeans";
             break;
+        case 3:
+            return "Jordan Spieth";
+            break;
     }
 }
+
+function quotedescrip() {
+    let numm = Math.floor(Math.random()*3);
+    switch (numm){
+        case 0:
+            return "Quote of the day";
+            break;
+        case 1:
+            return "Knowledge bomb of the week";
+            break;
+        case 2: 
+            return "WORDS";
+            break;
+    }
+}
+
+function randomQuotegen() {
+    return `${quotedescrip()}: ${choosePhrase()} by ${whoSaysIt()}`;
+}
+
+console.log(randomQuotegen());
